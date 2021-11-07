3c921f8c513ba2c66f67c72bfd454834a663a242
Dmitry Suzdalev <mail@dimsuz.ru>
2021-11-07 17:59:22 +0200

    Add LazyKeyCache

---
+++ library/src/main/kotlin/ru/dimsuz/yolk/LazyKeyCache.kt
@@ -0,0 +1,19 @@
+package ru.dimsuz.yolk
+
+class LazyKeyCache<K : Any, V>(
+  private val key: suspend () -> K,
+  expirePolicy: ExpirePolicy<K>,
+  fetch: suspend (key: K) -> V,
+  keyStore: KeyStore<K>,
+  valueStore: ValueStore<K, V>,
+  ticker: Ticker,
+  log: (() -> String) -> Unit = {}
+) {
+  private val _cache = Cache(expirePolicy, fetch, keyStore, valueStore, ticker, log)
+
+  suspend fun refresh() = _cache.refresh(key())
+  suspend fun load() = _cache.load(key())
+  suspend fun get(): V? = _cache.get(key())
+  suspend fun hasExpired() = _cache.hasExpired(key())
+  suspend fun invalidate() = _cache.invalidate(key())
+}
